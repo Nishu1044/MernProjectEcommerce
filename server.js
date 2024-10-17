e2e8ce3461b75ed065333579a5de915d37763ead
Nishu1044 <nishukumari3109@gmail.com>
2024-10-17 16:40:47 +0530

    done

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const PORT = process.env.PORT || 1000
 
 
 app.use(cors({
-   origin:"http://localhost:5173",
-   methods:["GET","POST","PUT","DELETE"],
+   origin:"*",
+//    methods:["GET","POST","PUT","DELETE"],
    credentials:true
 }))
 
